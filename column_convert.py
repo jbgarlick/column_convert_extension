40678f013b234d3c3a873acae8d3f79df558965f
Jared Garlick <jared.b.garlick@gmail.com>
2023-04-14 16:38:04 -0500

    Update column_convert.py

--- column_convert.py
+++ column_convert.py
@@ -4,11 +4,17 @@ def app():
     col1, col2, col3 = st.columns(3)
     with col1:
         convert_column = st.text_area('Convert column')
+        no_str = st.checkbox('Check here if you don\'t want quotations around each item')
         convert = st.button('Convert')
-        if convert:
+        if convert and not no_str:
             col_to_list = convert_column.splitlines()
             list_to_str = "','".join(col_to_list)
             final_for_query = "('" + list_to_str + "')"
+            st.text_area('output',final_for_query)
+        if convert and no_str:
+            col_to_list = convert_column.splitlines()
+            list_to_str = ",".join(col_to_list)
+            final_for_query = "(" + list_to_str + ")"
             st.text_area('output',final_for_query)
 
 
